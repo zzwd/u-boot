44b31ad1b28a92d041e149bab8ed83e0bc19e7a6
zzw <1600284146@qq.com>
2019-04-09 17:13:22 +0800

    add new ARCH xinjie

---
+++ include/configs/xin8i.h
@@ -0,0 +1,14 @@
+/*
+ * (C) Copyright 2014 Chen-Yu Tsai <wens@csie.org>
+ *
+ * Configuration settings for the Allwinner A23 (sun8i) CPU
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+
+
+#endif /* __CONFIG_H */
